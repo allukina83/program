08ec6547d13b80042fe7e9dd045f5cf132829888
Alina Lukina <alina_tula@rambler.ru>
2016-08-01 13:34:42 +0300

    fitst commit

---
+++ Lesson3_15/Lesson3_15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson3_15
+{
+    class Program
+    {
+        /*static void Main(string[] args)
+        {
+            Console.Write("x=");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y=");
+            double y = double.Parse(Console.ReadLine());
+            const int sizeX = 40;
+            const int sizeY = 40;
+
+            if (Math.Abs(x)<sizeX && Math.Abs(y)<sizeY)
+            {
+                Console.WriteLine("нет");
+            }
+            else
+            {
+                if (Math.Abs(x) > sizeX || Math.Abs(y) > sizeY)
+                {
+                    Console.WriteLine("да");
+                }
+                else
+                {
+                    Console.WriteLine("на границе");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
